899a55702354e6d39a4624cfc6d5d68872e84223
charity mahlangu <mahlangumayibongwe33@gmail.com>
2024-04-26 20:26:28 +0200

    added a search engine

---
+++ src/index.js
@@ -0,0 +1,9 @@
+function handlesearchsubmit(event){
+    event.preventDefault();
+    let searchinfo = document.querySelector("input")
+    let cityElement = document.querySelector("#city");
+    cityElement.innerHTML= searchinfo.value;
+}
+
+let searchformElement = document.querySelector("#searchinfo");
+searchformElement.addEventListener("submit", handlesearchsubmit)
