7665253c47615555b1608f790a79c56dd48ef253
charity mahlangu <mahlangumayibongwe33@gmail.com>
2024-04-28 15:17:45 +0200

    trying to get the api to work and show live data

--- src/index.js
+++ src/index.js
@@ -1,5 +1,17 @@
+function refreshWeather(response) {
+  let temperatureElement = document.querySelector("#temperature");
+  let temperature = response.data.temperature.current;
+  let cityElement = document.querySelector("#city");
 
+  cityElement.textContent = response.data.city;
+  temperatureElement.textContent = Math.round(temperature);
+}
 
+function searchCity(city) {
+  let apiKey = "6acodfbfaa832f9t2d0703f439a0aaeb";
+  let apiUrl = `https://api.shecodes.io/weather/v1/current?query=${city}&key=${apiKey}&units=metric`;
+  axios.get(apiUrl).then(refreshWeather);
+}
 function handleSearchSubmit(event) {
   event.preventDefault();
   let searchInput = document.querySelector(".input");
