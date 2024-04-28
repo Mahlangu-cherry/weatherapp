0e0976ebfbe49880bae382ba496d7ff14a02532f
charity mahlangu <mahlangumayibongwe33@gmail.com>
2024-04-28 14:52:16 +0200

    Update index.js

--- src/index.js
+++ src/index.js
@@ -1,18 +1,11 @@
 
-function searchCity(city) {
-  let apiKey = "6acodfbfaa832f9t2d0703f439a0aaeb";
-  let apiUrl = `https://api.shecodes.io/weather/v1/current?query=${city}&key=${apiKey}&units=metric`;
-  axios.get(apiUrl).then(refreshWeather);
-}
 
 function handleSearchSubmit(event) {
   event.preventDefault();
-  let searchInput = document.querySelector(".searchbutton");
-
-  searchCity(searchInput.value);
+  let searchInput = document.querySelector(".input");
+  let cityElement = document.querySelector("#city");
+  cityElement.textContent = searchInput.value;
 }
 
-let searchFormElement = document.querySelector("#search-form");
-searchFormElement.addEventListener("submit", handleSearchSubmit);
-
-searchCity("Johannesburg");
+let searchForm = document.querySelector("#searchform");
+searchForm.addEventListener("submit", handleSearchSubmit);
