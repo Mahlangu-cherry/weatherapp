0980ae5f2e2a14854bc7fea44504e9fb9488b5e0
charity mahlangu <mahlangumayibongwe33@gmail.com>
2024-04-28 13:57:53 +0200

    Update index.js

--- src/index.js
+++ src/index.js
@@ -1,25 +1,8 @@
-function refreshWeather(response) {
-  let tempElement = document.querySelector("#temp");
-  let temp = response.data.current.temp_c;
-  let cityElement = document.querySelector("#city");
-
-  cityElement.textContent = response.data.location.name;
-  tempElement.textContent = Math.round(temp);
-}
-
-function searchCity(city) {
-  let apiKey = "6acodfbfaa832f9t2d0703f439a0aaeb"; 
-  let apiUrl = `https://api.weatherapi.com/v1/current.json?key=${apiKey}&q=${city}`;
-  axios.get(apiUrl).then(refreshWeather);
-}
-
 function handleSearchSubmit(event) {
   event.preventDefault();
   let searchInput = document.querySelector(".input");
-  let city = searchInput.value.trim(); 
-  if (city !== "") {
-    searchCity(city);
-  }
+  let cityElement = document.querySelector("#city");
+  cityElement.textContent = searchInput.value;
 }
 
 let searchForm = document.querySelector("#searchform");
